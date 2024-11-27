e7a9aacbc5b0adff8b25f4a6b7f6649f4ca48877
vedanth515 <vedanthkarnataka@gmail.com>
2024-11-27 18:44:07 +0530

    operators task

---
+++ operators.js
@@ -0,0 +1,72 @@
+
+var age = 17;
+
+var userinput=prompt("enter your age")
+
+if(userinput>=17){
+    console.log("you are eligible to vote");
+    
+}else{
+    console.log("you are not eligible to vote");
+    
+}
+
+var age = 21;
+
+var userinput=prompt("enter your age here")
+
+if(userinput>=21){
+    console.log("you are eligible to marriage");
+    
+}else{
+    console.log("you are not eligible to marriage");
+    
+}
+
+var age = 18;
+
+var userinput=prompt("enter your age here")
+
+if(userinput>=18){
+    console.log("you are eligible to marriage");
+    
+}else{
+    console.log("you are not eligible to marriage");
+    
+}
+
+var age = 18;
+
+var userinput=prompt("apply for a driving license")
+
+if(userinput>=18){
+    console.log("you are eligible to apply driving license");
+    
+}else{
+    console.log("you are not eligible to apply driving license");
+    
+}
+
+var attendance = 75
+
+var userinput=prompt("student is eligible for an exam based on their attendance percentage")
+
+if(userinput>=75){
+    console.log("you are eligible for the exam");
+    
+}else{
+    console.log("you are not eligible for the exam");
+    
+}
+
+var age = 60;
+
+var userinput=prompt("if a person qualifies for a senior citizen discount")
+
+if(userinput>=60){
+    console.log("he will get senior citizen discount");
+    
+}else{
+    console.log("he will get not senior citizen discount");
+    
+}
